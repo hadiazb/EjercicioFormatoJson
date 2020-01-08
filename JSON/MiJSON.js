07b488d43f0534134bdb6ee64c94acc76dbf2c9d
hadiazb <hadiazb@unal.edu.co>
2020-01-08 17:52:12 -0500

    Ejercicio con formato json

---
+++ JSON/MiJSON.js
@@ -0,0 +1,24 @@
+var miJSON = [
+  {
+    "codigo": "001",
+    "nombre": "Calculo",
+  },
+  {
+    "codigo": "002",
+    "nombre": "Fisica",
+  },
+  {
+    "codigo": "003",
+    "nombre": "Algoritmo",
+  }
+];
+
+function leerJSON(json)
+{
+    var out = "";
+    var i;
+    for (var i = 0; i < json.length; i++) {
+      alert("codigo: " + json[i].codigo + "-" + "Nombre: " + json[i].nombre);
+    }
+}
+leerJSON(miJSON);
